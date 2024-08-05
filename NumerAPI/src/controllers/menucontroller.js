86ed533143acb4885bf84ccc62a56818017c1667
unknown <siwakchuhirun2004@gmail.com>
2024-08-06 00:00:44 +0700

    updatedata

--- NumerAPI/src/controllers/menucontroller.js
+++ NumerAPI/src/controllers/menucontroller.js
@@ -7,6 +7,20 @@ exports.menu = async(req,res) =>{
             return res.status(200).send(result);
         });
     }catch(error){
+        return res.status(200).send([
+            {
+              "id": 1,
+              "name": "Home",
+              "path": "/",
+              "cname": "sidetext"
+            },
+            {
+              "id": 2,
+              "name": "Root of equations",
+              "path": "/root_of_equations",
+              "cname": "sidetext"
+            }
+          ]);
         res.status(500).send(`${error}`);
     }
 };
