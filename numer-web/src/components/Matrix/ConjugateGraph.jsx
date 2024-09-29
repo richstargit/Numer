15daa60d601736e3908259abe823b043ef4850d6
unknown <siwakchuhirun2004@gmail.com>
2024-09-30 00:42:28 +0700

    edit conjugate graph

--- numer-web/src/components/Matrix/ConjugateGraph.jsx
+++ numer-web/src/components/Matrix/ConjugateGraph.jsx
@@ -25,6 +25,7 @@ const ConjugateGraph3D = (props) => {
     if(props.result.request == "success"){
       if(result.matrixA.length>=2&&result.matrixA[0].length>=2){
       simulateConjugateGradient();
+      
       }
     }
   }, [result]);
