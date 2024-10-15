fdf7edba92c1f14c4f4ae269e3151fd48b33694e
unknown <siwakchuhirun2004@gmail.com>
2024-10-15 23:23:41 +0700

    edit api

--- NumerAPI/src/router/routerser.js
+++ NumerAPI/src/router/routerser.js
@@ -4,6 +4,6 @@ const { menu } = require("../controllers/menucontroller");
 const router = express.Router();
 
 router.get('/getdata',bisection);
-router.get("/menu",menu)
+router.get("/getmenu",menu)
 
 module.exports = router;
