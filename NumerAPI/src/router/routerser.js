7a0194114aa60297ee9d1ab825a557fb440fbbcf
unknown <siwakchuhirun2004@gmail.com>
2024-10-16 11:06:44 +0700

    edit api

--- NumerAPI/src/router/routerser.js
+++ NumerAPI/src/router/routerser.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { rootofequations, rootsave } = require("../controllers/rootequation");
-const { menu } = require("../controllers/menucontroller");
 const router = express.Router();
 
 router.get('/root',rootofequations);
