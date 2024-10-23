bed18b5320257ecbedaafe41b08ed3fe199d382f
unknown <siwakchuhirun2004@gmail.com>
2024-10-23 17:01:32 +0700

    edit divided

--- numer-web/src/components/Differentiation/DifferenceResult.jsx
+++ numer-web/src/components/Differentiation/DifferenceResult.jsx
@@ -3,13 +3,13 @@ import { BlockMath } from 'react-katex';
 import 'katex/dist/katex.min.css';
 import { useEffect, useState } from "react";
 
-function DifferenceResult(props){
+function DifferenceResult(props) {
     const result = props.result;
 
-    const Requestdata = () =>{
-        if(result){
-            return(<>
-            <div style={{marginTop:"10px"}}>{result.mode} {result.Oh}</div>
+    const Requestdata = () => {
+        if (result) {
+            return (<>
+                <div style={{ marginTop: "10px" }}>{result.mode} {result.Oh}</div>
                 <BlockMath math={`f'(x) = ${result.result[0]} \\space\\space error = ${result.error[0]}\\%`}></BlockMath>
                 <BlockMath math={`f''(x) = ${result.result[1]} \\space\\space error = ${result.error[1]}\\%`}></BlockMath>
                 <BlockMath math={`f'''(x) = ${result.result[2]} \\space\\space error = ${result.error[2]}\\%`}></BlockMath>
@@ -21,8 +21,8 @@ function DifferenceResult(props){
 
     return (
         <>
-            <div style={{width:"80%",background:"rgb(255, 255, 255)",height:"",margin:"auto",paddingTop:"10px",paddingBottom:"10px",marginTop:"15px",borderRadius:"15px",border:"2px solid #272829",marginBottom:"15px",overflowX: "auto"}}>
-                <div style={{fontSize:"22px"}}>Solution</div>
+            <div style={{ width: "80%", background: "rgb(255, 255, 255)", height: "", margin: "auto", paddingTop: "10px", paddingBottom: "10px", marginTop: "15px", borderRadius: "15px", border: "2px solid #272829", marginBottom: "15px", overflowX: "auto" }}>
+                <div style={{ fontSize: "22px" }}>Solution</div>
                 {Requestdata()}
             </div>
         </>
