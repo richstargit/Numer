dcaecc57cc9abd231a3c6710a721605ad412593b
unknown <siwakchuhirun2004@gmail.com>
2024-10-25 00:23:30 +0700

    database root

--- NumerAPI/src/router/routerser.js
+++ NumerAPI/src/router/routerser.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { rootofequations, rootsave } = require("../controllers/api");
+const { rootofequations, rootsave, linear } = require("../controllers/api");
 const router = express.Router();
 
 router.get('/root',rootofequations);
 router.post('/rootsave',rootsave);
+router.get('/linear',linear);
 
 module.exports = router;
