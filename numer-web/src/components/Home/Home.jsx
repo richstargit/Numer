b6258e27582f5f9fe883a9a25db1c5a77e008209
unknown <siwakchuhirun2004@gmail.com>
2024-10-25 00:34:48 +0700

    database linear

--- numer-web/src/components/Home/Home.jsx
+++ numer-web/src/components/Home/Home.jsx
@@ -8,12 +8,12 @@ function Home(){
             <div>
                 <h1><b>NUMERICAL METHOD</b></h1>
             </div>
-            <div style={{marginTop:"35px",fontSize:"36px"}}>Root of equations</div>
-            <div style={{width:"80%",marginLeft:"auto",marginRight:"auto",marginTop:"15px"}}>
+            <div style={{ marginTop: "35px", fontSize: "36px" }}>Root of equations</div>
+            <div style={{ width: "80%", marginLeft: "auto", marginRight: "auto", marginTop: "15px" }}>
                 <StickyHeadTable></StickyHeadTable>
             </div>
-            <div style={{marginTop:"35px",fontSize:"36px"}}>Linear Algebraic</div>
-            <div style={{width:"80%",marginLeft:"auto",marginRight:"auto",marginTop:"15px"}}>
+            <div style={{ marginTop: "35px", fontSize: "36px" }}>Linear Algebraic</div>
+            <div style={{ width: "80%", marginLeft: "auto", marginRight: "auto", marginTop: "15px" }}>
                 <TalbeLinear></TalbeLinear>
             </div>
         </div>
