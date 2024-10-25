ed94be156173798615befd3292153f43df735ec4
unknown <siwakchuhirun2004@gmail.com>
2024-10-25 19:04:12 +0700

    database inter

--- NumerAPI/src/controllers/api.js
+++ NumerAPI/src/controllers/api.js
@@ -5,7 +5,7 @@ const pool = new Pool({
 
 exports.rootofequations = async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM rootofequation');
+        const result = await pool.query('SELECT * FROM rootofequation order by id');
         
         return res.status(200).send({
             request: "success",
@@ -44,7 +44,7 @@ exports.rootsave = async (req, res) => {
 
 exports.linear = async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM linear');
+        const result = await pool.query('SELECT * FROM linear order by id');
         
         return res.status(200).send({
             request: "success",
@@ -82,6 +82,53 @@ exports.linearsave = async (req, res) => {
         return res.status(200).send({
             data: values,
             request: "success",
+        });
+
+    } catch (error) {
+        console.error('Database query error', error);
+        res.status(500).send(`${error}`);
+    }
+};
+
+exports.inter = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM Interpolation order by id');
+        
+        return res.status(200).send({
+            request: "success",
+            data: result.rows,
+        });
+
+    } catch (error) {
+        console.error('Database query error', error);
+        res.status(500).send(`${error}`);
+    }
+};
+
+exports.intersave = async (req, res) => {
+    try {
+        const { tablex, tabley, mode, result, n, xsize, resultx } = req.body;
+
+        const query = `
+            INSERT INTO Interpolation (tablex, tabley, mode, result, n, xsize, resultx)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+        `;
+
+        const values = [
+            tablex,
+            tabley,
+            mode,
+            result,
+            n,
+            xsize,
+            resultx
+        ];
+
+        await pool.query(query, values);
+
+        return res.status(200).send({
+            request: "success",
+            data: values,
         });
 
     } catch (error) {
--- NumerAPI/src/router/routerser.js
+++ NumerAPI/src/router/routerser.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { rootofequations, rootsave, linear, linearsave } = require("../controllers/api");
+const { rootofequations, rootsave, linear, linearsave, inter, intersave } = require("../controllers/api");
 const router = express.Router();
 
 router.get('/root',rootofequations);
 router.post('/rootsave',rootsave);
 router.get('/linear',linear);
 router.post('/linearsave',linearsave);
+router.get('/inter',inter);
+router.post('/intersave',intersave);
 
 module.exports = router;
