946d615ca818f8714ae50b8a31b34d26197b10e9
unknown <siwakchuhirun2004@gmail.com>
2024-11-01 23:00:12 +0700

    docker

--- numer-web/src/components/Home/TableRoot.jsx
+++ numer-web/src/components/Home/TableRoot.jsx
@@ -82,7 +82,7 @@ export default function StickyHeadTable() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:3000/api/root');
+        const response = await fetch('https://numer-api.vercel.app/api/root');
         const jsonData = await response.json();
         
         if (jsonData.request === 'success') {
--- numer-web/src/components/Integration/IntegrationData.jsx
+++ numer-web/src/components/Integration/IntegrationData.jsx
@@ -59,7 +59,7 @@ function IntegrationData({onDataChange}){
             }).then(async (res) => {
                 if (res.isConfirmed) {
                     setLoading(true);
-                    const response = await fetch('http://localhost:3000/api/integrationsave', {
+                    const response = await fetch('https://numer-api.vercel.app/api/integrationsave', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
